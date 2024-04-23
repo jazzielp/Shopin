44f32e148b68e85b38cc4fc235d60609daa8191b
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-04-23 12:09:32 -0700

    ✨ Enrutamiento con React Router DOM

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,21 +1,31 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import { Home } from '../Home'
 import { MyAccount } from '../MyAccount'
 import { MyOrder } from '../MyOrder'
 import { MyOrders } from '../MyOrders'
 import { NotFound } from '../NotFound'
 import { SignIn } from '../SignIn'
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/home', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/signin', element: <SignIn /> },
+    { path: '*', element: <NotFound /> }
+
+  ])
+
+  return routes
+}
+
 function App () {
   return (
-    <>
-      <h1 className='text-red-200'>Hello world</h1>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 

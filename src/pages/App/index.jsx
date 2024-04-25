c991989bf4d78e25868249f807515cbb329d5799
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-04-25 16:52:19 -0700

    Contexto global de la aplicación

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartProvider } from '../../Context'
 import { Home } from '../Home'
 import { MyAccount } from '../MyAccount'
 import { MyOrder } from '../MyOrder'
@@ -24,10 +25,12 @@ const AppRoutes = () => {
 
 function App () {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <AppRoutes />
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <NavBar />
+        <AppRoutes />
+      </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
